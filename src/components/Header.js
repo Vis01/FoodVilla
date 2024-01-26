eb8167f340ced122ef16c8da3412f64bdd6255f1
Vishnu Jangid <vishnu.j8503@gmail.com>
2024-01-26 18:44:36 +0530

    Routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,28 +10,31 @@ import { Link } from 'react-router-dom';
 const Header=()=>{
    const [isSignin,setisSignin]=useState(false);
     return (
-        <div className="border-2  flex m-2  justify-between  ">
+        <div className="relative h-20 w-full border-2  flex m-2  justify-between  ">
             <Title/>
-            <div className="hidden md:flex  m-5">               
+            <div className="">               
                 <ul className=" flex text-xl font-bold justify-between mg-2 mt-3">
-                <li className=" mx-3 px-1 flex hover:text-orange-400 cursor-pointer "
-
-                >
-                 <Link to="/about" >   About Us</Link> 
+                <li className=" mx-3 px-1 flex justify-center hover:text-orange-400 cursor-pointer ">
+                   <Link to="/about">
+                   About Us
+                   </Link>
+                   
                 </li>
-                <li className=" mx-3 px-1 flex  hover:text-orange-400 cursor-pointer ">
-                    <img className='m-1 mt-0 w-8' src={Offericon}/>
-                    Offer
+                <li className=" mx-3 px-1 flex item-center hover:text-orange-400 cursor-pointer ">
+                    <img className=' mt-0 w-8' src={Offericon}/>
+                     Offer
                 </li>
-                <li className=" mx-3 px-1 flex  hover:text-orange-400 cursor-pointer">
-                    <img className='m-1 mt-0 w-7' src={Helpicon}/>
+                <li className=" mx-3 px-1   hover:text-orange-400 cursor-pointer">   
+                  <Link className='flex items-center' to="/contact" >
+                  <img className='m-1 mt-0 w-7' src={Helpicon}/>
                     Contact Us
+                  </Link>    
                 </li>
-                <li className=" mx-3 px-1 flex hover:text-orange-400 cursor-pointer"  >
+                <li className=" mx-3 px-1 flex items-center  hover:text-orange-400 cursor-pointer"  >
                     <img className='m-1 mt-0 w-7' src={Carticon}/>
                     Cart
                 </li>
-                <li className=" mx-3 px-1 flex  hover:text-orange-400 cursor-pointer"
+                <li className=" mx-3 px-1 flex items-center    hover:text-orange-400 cursor-pointer"
                     onClick={()=>{
                         setisSignin(!isSignin);
                     }}
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Logo from "../utills/icons/logo.png";
+import { Link } from 'react-router-dom';
 const Title = () => {
   return (
     <div className='flex'>
-        <h1 className="ml-3 my-4 text-2xl  font-bold justify-center ">
-            Food Villa
+        <h1 className="ml-3 my-4 text-2xl  font-bold justify-center  hover:text-orange-400 cursor-pointer ">
+           <Link to="/"> Food Villa</Link> 
         </h1>
         <img className='w-10 ml-8 my-4' alt=" logo" src={Logo}/>
-        
-       
     </div>
   )
 }
